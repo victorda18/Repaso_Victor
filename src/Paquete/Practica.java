eec5d1c635c452055eb94dbafa349b93b7c7adae
Ymirko <Ymirko@DESKTOP-V3P4O0Q>
2025-05-12 08:58:00 +0200

    cambio12/05

--- src/Paquete/Practica.java
+++ src/Paquete/Practica.java
@@ -5,6 +5,7 @@ public class Practica {
 	public static void main(String[] args) {
 		System.out.println("Esto es un ejemplo");
 		System.out.println("Esto es una Rama1");
+		System.out.println("Esto es una Rama1");
 		
 	}
 
